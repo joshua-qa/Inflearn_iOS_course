527bd36bd044c8cc102cb84738a24685b36f44be
jgchoiqa <jgchoi.qa@gmail.com>
2017-06-04 23:14:20 +0900

    struct_tutorial add

---
+++ struct_tutorial.swift
@@ -0,0 +1,10 @@
+struct Student {
+    var name:String
+    var age:Int
+    var height:Int
+}
+
+var student1 = Student(name: "Rabbit", age: 1, height: 40)
+var student2 = Student(name: "Sagiri", age: 2, height: 50)
+
+print(student2.name)
