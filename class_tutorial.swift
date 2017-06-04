57891313f1d7449e6098eb84ee323562401c9d94
jgchoiqa <jgchoi.qa@gmail.com>
2017-06-04 23:07:07 +0900

    class_tutorial modify

--- class_tutorial.swift
+++ class_tutorial.swift
@@ -51,3 +51,27 @@ let train = Train()
 train.makeNoise()
 
 bicycle.makeNoise()
+
+class Car:Vehicle {
+    var gear = 1
+    override var description: String {
+        return super.description + " in gear \(gear)"
+    }
+}
+
+let car = Car()
+car.currentSpeed = 25.0
+car.gear = 3
+print("Car : \(car.description)")
+
+class AutomaticCar:Car {
+    override var currentSpeed: Double {
+        didSet {
+            gear = Int(currentSpeed / 10) + 1
+        }
+    }
+}
+
+let automatic = AutomaticCar()
+automatic.currentSpeed = 30.0
+print("AutomaticCar : \(automatic.description)")
