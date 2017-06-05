b552e80172441c256fea6d7a462b340f5758297e
jgchoiqa <jgchoi.qa@gmail.com>
2017-06-05 14:01:36 +0900

    optional_tutorial add

---
+++ optional_tutorial.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Optional {
+    var optionalString:String?
+    
+    func hello() {
+        print(optionalString==nil)
+    }
+}
+
+var option = Optional()
+
+option.hello()
