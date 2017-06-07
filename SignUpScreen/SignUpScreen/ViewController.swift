d9c4180159d6074c0c8b5cea41f0030d2a7ce8d0
jgchoiqa <jgchoi.qa@gmail.com>
2017-06-07 23:25:47 +0900

    SignUpScreen add

---
+++ SignUpScreen/SignUpScreen/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  SignUpScreen
+//
+//  Created by jgchoiqa on 2017. 6. 7..
+//  Copyright © 2017년 Joshua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var idTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var telTextField: UITextField!
+    @IBOutlet var blogTextField: UITextField!
+
+    @IBOutlet var resultTextView: UITextView!
+    
+    @IBAction func signUpAction(_ sender: AnyObject) {
+        resultTextView.text = "\(nameTextField.text!) 님 가입을 축하합니다."
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //print("textFieldShouldReturn")
+        nameTextField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
