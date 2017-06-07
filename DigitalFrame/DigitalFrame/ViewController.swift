28ad8bf699bb5983c0ba72af33dcf6a8d9534d0a
jgchoiqa <jgchoi.qa@gmail.com>
2017-06-07 17:08:10 +0900

    DigitalFrame add

---
+++ DigitalFrame/DigitalFrame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DigitalFrame
+//
+//  Created by jgchoiqa on 2017. 6. 7..
+//  Copyright © 2017년 Joshua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
