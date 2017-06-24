e5639f93ea5d7cb0235d9a74ec113890e0f1166a
jgchoiqa <jgchoi.qa@gmail.com>
2017-06-24 18:26:41 +0900

    datePicker 동작 구현

--- FlightBooking/FlightBooking/ViewController.swift
+++ FlightBooking/FlightBooking/ViewController.swift
@@ -17,12 +17,45 @@ class ViewController: UIViewController {
     @IBOutlet var returnDateLabel: UILabel!
     @IBOutlet var returnDateButton: UIButton!
     
-    @IBAction func showReturnDateAction(_ sender: Any) {
+    @IBAction func showReturnDateAction(_ sender: AnyObject) {
+        returnDateLabel.isHidden = !(sender as! UISwitch).isOn
+        returnDateButton.isHidden = !(sender as! UISwitch).isOn
+    }
+    
+    var buttonTag:Int = 1
+    @IBAction func selectedDateAction(_ sender: Any) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YY-MM-d hh:mma"
+        
+        let dateString = formatter.string(from: selectDatePicker.date)
+        
+        if buttonTag == 1 {
+            departureDateButton.setTitle(dateString, for: UIControlState())
+        } else {
+            returnDateButton.setTitle(dateString, for: UIControlState())
+        }
+    }
+    
+    @IBAction func showDatePickerAction(_ sender: Any) {
+        if selectDatePicker.isHidden == false {
+            selectDatePicker.isHidden = true
+        } else {
+            selectDatePicker.isHidden = false
+        }
+        
+        buttonTag = (sender as! UIButton).tag
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         returnDateLabel.isHidden = true;
+        returnDateButton.isHidden = true;
+        selectDatePicker.isHidden = true;
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        selectDatePicker.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
